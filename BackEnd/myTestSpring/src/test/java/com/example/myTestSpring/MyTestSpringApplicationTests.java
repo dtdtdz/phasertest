2c3eb6110b02a34832a17be423b3a6af135568ac
dtdtdz <dtdtdz@gmail.com>
2024-02-27 17:54:29 +0900

    phaser

---
+++ BackEnd/myTestSpring/src/test/java/com/example/myTestSpring/MyTestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myTestSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
